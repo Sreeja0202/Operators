346ab24552ec5e27601492e990075e29c127ed5e
Sreeja0202 <106142975+Sreeja0202@users.noreply.github.com>
2022-05-24 16:51:16 +0530

    Original

---
+++ operator.js
@@ -0,0 +1,81 @@
+let a = 10;
+let b = 5;
+//let sum = a+b; //Addition
+// console.log("Sum =" +sum);
+// let sub = a-b;
+// let mul = a*b;
+// let div = a/b;
+// let rem = a%b;
+// console.log("Differnce =" +sub);
+// console.log("Product =" +mul);
+// console.log("Dividend =" +div);
+// console.log("Remainder =" +rem);
+
+// let c = ++a;
+// let d = b++;
+// console.log(c);
+// console.log(d);
+
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a<=b);
+
+// console.log(a+=3);
+// console.log(a-=3);
+// console.log(a*=3);
+// console.log(a/=5);
+
+// let x=true;
+// let y=false;
+// console.log(x&&y);
+// console.log(x||y);
+// console.log(!x);
+// console.log(!y);
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+// console.log(!b);
+
+// let m=1001;
+// let n=1010;
+// console.log(m&n);
+// console.log(m|n);
+
+// let c = 3;
+// console.log((a+b)*c);
+// console.log(a+b*c);
+
+// if(a>b)
+// {
+//     console.log("A is greater");
+// }
+// else{
+//     console.log("B is greater");
+// }
+// let s = 1;
+// for(i=1; i<=5; ++i)
+// {
+//     s= s*i;
+// }
+// console.log(s);
+// let s = 1;
+// let i = 1;
+// while(i<=5)
+// {
+//     s = s*i;
+//     i++;
+// }
+// console.log(s);
+
+let i = 1;
+let s = 1;
+do{
+    s=s*i;
+    i++;
+    console.log(s);
+}
+while(i<=5);
